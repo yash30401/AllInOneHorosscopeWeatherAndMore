15ceeff4925cb2247e949aff70d14bf4b86ae6da
yash30401 <yashveersingh30401@gmail.com>
2022-06-29 20:11:02 +0530

    Initial commit

---
+++ app/src/main/java/com/devyash/allinone_horosscopeweatherandmore/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.devyash.allinone_horosscopeweatherandmore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+
+}
